1c646e9df4cadfe182be7ad52de41af331d1b74b
Muhammad Bilal Azaad <mr.bilal2066@gmail.com>
2024-12-30 18:06:53 +0500

    feat: return tenant specific signup template

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path("signup-templates/<int:pk>/", views.SignUpTemplateView.as_view()),
-    path("signup-templates/", views.SignUpTemplateView.as_view()),
+    path("my_signup_template/", views.my_signup_template),
 ]
--- app/views.py
+++ app/views.py
@@ -1,16 +1,13 @@
 from rest_framework import generics, viewsets, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from customers.serializers import TemplateSerializer
 from customers import models as CustomerModels
 
 
-class SignUpTemplateView(
-    generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin
-):
-    queryset = CustomerModels.SignUpTemplate.objects.all()
-    serializer_class = TemplateSerializer
-
-    def get(self, request, *args, **kwargs):
-        if "pk" in kwargs:
-            return self.retrieve(request, *args, **kwargs)
-        return self.list(request, *args, **kwargs)
+@api_view(['GET'])
+def my_signup_template(request):
+    if request.tenant.signup_template:
+        return Response(request.tenant.signup_template.fields)
+    return Response("No template found")
