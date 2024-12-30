c03ec0afcaa097093f6beacf8bc1ec21c6a5129d
Muhammad Bilal Azaad <mr.bilal2066@gmail.com>
2024-12-30 15:51:09 +0500

    fix: list & retrieve view

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path("signup-templates/<int:pk>", views.SignUpTemplateView.as_view()),
+    path("signup-templates/<int:pk>/", views.SignUpTemplateView.as_view()),
     path("signup-templates/", views.SignUpTemplateView.as_view()),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, mixins
 
 from customers.serializers import TemplateSerializer
 from customers import models as CustomerModels
 
 
-class SignUpTemplateView(generics.ListAPIView):
+class SignUpTemplateView(
+    generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin
+):
     queryset = CustomerModels.SignUpTemplate.objects.all()
     serializer_class = TemplateSerializer
+
+    def get(self, request, *args, **kwargs):
+        if "pk" in kwargs:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
